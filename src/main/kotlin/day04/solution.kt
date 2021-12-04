4496022ada5329ffb32ecea4944bf2c0872975b8
kubo <jsodpad@gmail.com>
2021-12-04 15:52:38 +0100

    day 4 + remove task definitions

---
+++ src/main/kotlin/day04/solution.kt
@@ -0,0 +1,61 @@
+package day04
+
+import java.io.File
+
+fun main() {
+    var drawnNumbers: List<Int> = listOf()
+    var bingoBoards: List<BingoBoard> = listOf()
+    val inputLines = File("src/main/kotlin/day04/input.txt").readLines()
+    drawnNumbers = inputLines.take(1).single().split(",").map { it.toInt() }
+
+    bingoBoards = inputLines.drop(1)
+        .filter { it.isNotEmpty() }
+        .chunked(5)
+        .map { BingoBoard(it.map { it.split(" ").filter { it.isNotEmpty() }.map { it.toInt() } }.toList()) }
+        .toList()
+
+    for (drawnNumber in drawnNumbers) {
+        val indexOfWinningBoard = bingoBoards.indexOfFirst { it.isWinning(drawnNumber) }
+        if (indexOfWinningBoard != -1) {
+            println(drawnNumber * bingoBoards[indexOfWinningBoard].unmarkedNumbers().sum())
+            break
+        }
+    }
+    val notYetWonBoards = ArrayList(bingoBoards)
+    val wonBoards = mutableListOf<BingoBoard>()
+    for (drawnNumber in drawnNumbers) {
+        val wonBoardsForDrawnNumber = notYetWonBoards.filter { it.isWinning(drawnNumber) }
+        wonBoards.addAll(wonBoardsForDrawnNumber)
+        notYetWonBoards.removeAll(wonBoardsForDrawnNumber)
+        if (notYetWonBoards.isEmpty()) {
+            println(drawnNumber * wonBoards.last().unmarkedNumbers().sum())
+            break
+        }
+    }
+
+}
+
+class BingoBoard(private val board: List<List<Int>>) {
+    private val markedNumbers: MutableList<Int> = mutableListOf()
+
+    fun isWinning(drawnNumber: Int): Boolean {
+        board.forEach {
+            if (it.contains(drawnNumber)) {
+                markedNumbers.add(drawnNumber)
+            }
+        }
+        return checkBoard()
+    }
+
+    private fun checkBoard(): Boolean {
+        return board.any {
+            it.all { it in markedNumbers }
+        } || (0..4).map { column ->
+            board.map { it[column] }
+        }.any {
+            it.all { it in markedNumbers }
+        }
+    }
+
+    fun unmarkedNumbers(): List<Int> = board.flatten().filter { it !in markedNumbers }
+}

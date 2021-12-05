f592c3146d412813acbb8d7640061032de9ce541
kubo <jsodpad@gmail.com>
2021-12-05 14:33:52 +0100

    day 5 "pimp"

--- src/main/kotlin/day05/solution.kt
+++ src/main/kotlin/day05/solution.kt
@@ -4,38 +4,36 @@ import java.io.File
 import kotlin.math.abs
 
 fun main() {
+    val segmentPattern = Regex("""(\d+),(\d+) -> (\d+),(\d+)""")
     val inputLines = File("src/main/kotlin/day05/input.txt").readLines()
-
     val segments = inputLines
-        .mapNotNull { Regex("""(\d+),(\d+) -> (\d+),(\d+)""").matchEntire(it) }
+        .mapNotNull { segmentPattern.matchEntire(it) }
         .map {
             Segment(
                 Point(it.groupValues[1].toInt(), it.groupValues[2].toInt()),
                 Point(it.groupValues[3].toInt(), it.groupValues[4].toInt())
             )
         }
-    val ventMap = segments
+    val dangerousVentCount = segments.asSequence()
         .filter { it.isLateral() }
-        .fold(mutableMapOf<Point, Int>()) { acc, segment ->
-            segment.getSegmentPoints().forEach {
-                acc.compute(it) { _, v -> v?.inc() ?: 1 }
-            }
-            acc
-        }
-    println(ventMap.values.count { it > 1 })
+        .dangerousVentCount()
+    println(dangerousVentCount)
 
-    val ventMap2 = segments
-        .fold(mutableMapOf<Point, Int>()) { acc, segment ->
-            segment.getSegmentPoints().forEach {
-                acc.compute(it) { _, v -> v?.inc() ?: 1 }
-            }
-            acc
-        }
-    println(ventMap2.values.count { it > 1 })
+    val dangerousVentCount2 = segments.asSequence()
+        .dangerousVentCount()
+    println(dangerousVentCount2)
 }
 
+fun Sequence<Segment>.dangerousVentCount() = this
+    .map { it.getSegmentPoints() }
+    .flatten()
+    .groupBy { it }
+    .count { it.value.size > 1 }
+
 data class Segment(val p1: Point, val p2: Point) {
+
     fun isLateral(): Boolean = p1.x == p2.x || p1.y == p2.y
+
     fun getSegmentPoints(): List<Point> {
         val points = mutableListOf<Point>()
         var lastPoint: Point = p1
@@ -54,7 +52,9 @@ data class Segment(val p1: Point, val p2: Point) {
 
     private fun getLength(): Int = maxOf(abs(p2.x - p1.x), abs(p2.y - p1.y))
 }
+
 data class Point(val x: Int, val y:Int) {
     operator fun plus(vector: Vector): Point = Point(x + vector.dx, y + vector.dy)
 }
+
 data class Vector(val dx: Int, val dy: Int)

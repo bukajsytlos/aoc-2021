eca276085de6394edb0bd9e5b4b88f498b0ece2e
kubo <jsodpad@gmail.com>
2021-12-06 11:00:02 +0100

    day 6 "pimp"

--- src/main/kotlin/day06/solution.kt
+++ src/main/kotlin/day06/solution.kt
@@ -1,34 +1,31 @@
 package day06
 
 import java.io.File
-import java.math.BigInteger
 
 private const val REPRODUCTION_INTERVAL = 7
 
 fun main() {
-    val fishAges = File("src/main/kotlin/day06/input.txt").readLines().first().split(",").map { it.toInt() }
-    val fishCountByTimer = fishAges.fold(Array(9) { BigInteger.ZERO }) { acc, fishAge ->
-        acc[fishAge]++
+    val fishReproductionTimers = File("src/main/kotlin/day06/input.txt").readLines().first().split(",").map { it.toInt() }
+    val fishCountByTimer = fishReproductionTimers.fold(Array(9) { 0L }) { acc, fishReproductionTimer ->
+        acc[fishReproductionTimer]++
         acc
     }
-
-    val fishCountByTimer2 = fishCountByTimer.copyOf()
 
     for (day in 0..79) {
         calculateFishCount(day, fishCountByTimer)
     }
     println(fishCountByTimer.asSequence().sumOf { it })
 
-    for (day in 0..255) {
-        calculateFishCount(day, fishCountByTimer2)
+    for (day in 80..255) {
+        calculateFishCount(day, fishCountByTimer)
     }
-    println(fishCountByTimer2.asSequence().sumOf { it })
+    println(fishCountByTimer.asSequence().sumOf { it })
 }
 
-private fun calculateFishCount(day: Int, fishCountByTimer: Array<BigInteger>) {
-    val reproductionDay = day % REPRODUCTION_INTERVAL
-    val newFishes = fishCountByTimer[reproductionDay]
-    fishCountByTimer[reproductionDay] += fishCountByTimer[7]
+private fun calculateFishCount(day: Int, fishCountByTimer: Array<Long>) {
+    val reproductionDayTimer = day % REPRODUCTION_INTERVAL
+    val newFishCount = fishCountByTimer[reproductionDayTimer]
+    fishCountByTimer[reproductionDayTimer] += fishCountByTimer[7]
     fishCountByTimer[7] = fishCountByTimer[8]
-    fishCountByTimer[8] = newFishes
+    fishCountByTimer[8] = newFishCount
 }

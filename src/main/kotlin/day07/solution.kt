5978482959449e3fbbb9e1ebaff2d0f6e3a87146
kubo <jsodpad@gmail.com>
2021-12-07 09:30:51 +0100

    day 7 "pimp"

--- src/main/kotlin/day07/solution.kt
+++ src/main/kotlin/day07/solution.kt
@@ -2,21 +2,21 @@ package day07
 
 import java.io.File
 import kotlin.math.abs
+import kotlin.math.roundToInt
 
 fun main() {
     val crabSubmarinePositions =
         File("src/main/kotlin/day07/input.txt").readLines().first().split(",").map { it.toInt() }
-    println(calculateFuelSpent(crabSubmarinePositions) { it })
-    println(calculateFuelSpent(crabSubmarinePositions) { it * (it + 1) / 2 })
+
+    val medianDistance = crabSubmarinePositions.sorted()[crabSubmarinePositions.size / 2]
+    println(calculateFuelSpent(crabSubmarinePositions, medianDistance) { it })
+
+    val meanDistance = crabSubmarinePositions.average().roundToInt()
+    val minFuelSpent = ((meanDistance - 1)..(meanDistance + 1)) //correct answer should be around mean
+        .map { position -> calculateFuelSpent(crabSubmarinePositions, position) { it * (it + 1) / 2 } }
+        .minOf { it }
+    println(minFuelSpent)
 }
 
-private fun calculateFuelSpent(crabSubmarinePositions: List<Int>, fuelFormula: (Int) -> Int): Int {
-    var minFuelSpent = Int.MAX_VALUE
-    for (i in 0.. crabSubmarinePositions.maxOf { it }) {
-        val distanceToPosition = crabSubmarinePositions.sumOf { fuelFormula.invoke(abs(it - i)) }
-        if (distanceToPosition < minFuelSpent) {
-            minFuelSpent = distanceToPosition
-        }
-    }
-    return minFuelSpent
-}
+private fun calculateFuelSpent(crabSubmarinePositions: List<Int>, position: Int, fuelFormula: (Int) -> Int): Int =
+    crabSubmarinePositions.sumOf { fuelFormula.invoke(abs(it - position)) }

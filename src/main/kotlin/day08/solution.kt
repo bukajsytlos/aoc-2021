f38446a3c48e5ca5c4b7278efefdeaa43be92e55
kubo <jsodpad@gmail.com>
2021-12-08 19:52:34 +0100

    day 8 documentation

--- src/main/kotlin/day08/solution.kt
+++ src/main/kotlin/day08/solution.kt
@@ -14,32 +14,44 @@ fun main() {
 
     println(signalToOutputList.flatMap { it.second }.count { it.size in setOf(2, 3, 4, 7) })
 
-    println(signalToOutputList.sumOf { determineNumber(it.first, it.second) })
+    println(signalToOutputList.sumOf { decodeNumber(it.first, it.second) })
 
 }
 
-fun determineNumber(uniqueSignalPatterns: List<SortedSet<Char>>, unknownNumber: List<SortedSet<Char>>): Int {
+/**
+ * Segments
+ *   111
+ *  2   3
+ *  2   3
+ *   444
+ *  5   6
+ *  5   6
+ *   777
+ */
+fun decodeNumber(uniqueSignalPatterns: List<SortedSet<Char>>, unknownNumber: List<SortedSet<Char>>): Int {
     val segmentsToDigit = HashMap<SortedSet<Char>, Int>()
     segmentsToDigit[uniqueSignalPatterns.first { it.size == 2 }] = 1
     segmentsToDigit[uniqueSignalPatterns.first { it.size == 3 }] = 7
     segmentsToDigit[uniqueSignalPatterns.first { it.size == 4 }] = 4
     segmentsToDigit[uniqueSignalPatterns.first { it.size == 7 }] = 8
 
-    val fifthSegmentChar = uniqueSignalPatterns.flatten()
-            .groupBy { it }
-            .mapValues { it.value.size }
+    val segmentFrequencies = uniqueSignalPatterns.flatten()
+        .groupBy { it }
+        .mapValues { it.value.size }
+    val fifthSegmentChar = segmentFrequencies
             .filter { it.value == 4 }.map { it.key }
             .first()
     val unknownFiveSegmentSignals = uniqueSignalPatterns.filter { it.size == 5 }.toMutableList()
     val unknownSixSegmentSignals = uniqueSignalPatterns.filter { it.size == 6 }.toMutableList()
     var segmentsRepresentingFive: SortedSet<Char>? = null
     var segmentsRepresentingSix: SortedSet<Char>? = null
+    //5 with fifth segment is 6
     for (fiveSegmentSignal in unknownFiveSegmentSignals) {
-            val tmpSet = fiveSegmentSignal.toSortedSet()
-            tmpSet.add(fifthSegmentChar)
-            if (unknownSixSegmentSignals.contains(tmpSet)) {
+            val fiveSegmentSignalWithFifthSegment = fiveSegmentSignal.toSortedSet()
+            fiveSegmentSignalWithFifthSegment.add(fifthSegmentChar)
+            if (unknownSixSegmentSignals.contains(fiveSegmentSignalWithFifthSegment)) {
                 segmentsRepresentingFive = fiveSegmentSignal
-                segmentsRepresentingSix = tmpSet
+                segmentsRepresentingSix = fiveSegmentSignalWithFifthSegment
                 unknownFiveSegmentSignals.remove(segmentsRepresentingFive)
                 unknownSixSegmentSignals.remove(segmentsRepresentingSix)
                 break
